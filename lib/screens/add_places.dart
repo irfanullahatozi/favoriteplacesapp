986dc1044732fdbf291019c60f63aa4ef285847c
Irfan Ullah <irfan.ullah@arcelik.com>
2023-10-27 12:00:55 +0500

    adding new screens

---
+++ lib/screens/add_places.dart
@@ -0,0 +1,63 @@
+import 'package:favoriteplacesapp/providers/user_places.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() {
+    return _AddPlaceScreen();
+  }
+}
+
+class _AddPlaceScreen extends ConsumerState<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  void savePlace() {
+    final enterdText = _titleController.text;
+    if (enterdText.isEmpty) {
+      return;
+    }
+    ref.read(userPlacesProvider.notifier).addPlace(enterdText);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new Place'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Title',
+              ),
+              controller: _titleController,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton.icon(
+                onPressed: savePlace,
+                icon: const Icon(Icons.add),
+                label: const Text('Add Place'))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/places_list_item.dart
@@ -0,0 +1,40 @@
+import 'package:favoriteplacesapp/models/place.dart';
+import 'package:favoriteplacesapp/screens/places_detail.dart';
+import 'package:flutter/material.dart';
+
+class PlaceList extends StatelessWidget {
+  PlaceList({super.key, required this.places});
+
+  final List<Place> places;
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return Center(
+        child: Text(
+          "No places added yet",
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      );
+    }
+    return ListView.builder(
+      itemBuilder: (ctx, index) => ListTile(
+        title: Text(
+          places[index].title,
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailScreen(place: places[index]),
+            ),
+          );
+        },
+      ),
+      itemCount: places.length,
+    );
+  }
+}
